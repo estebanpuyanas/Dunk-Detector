8a3173de4f006f1baacdf89e19c87c13f255a9c2
Jrodrigo06 <jeromerodrigo06@gmail.com>
2025-04-13 12:05:04 -0400

    Updated DataAnalyst HomePage and renamed other pages

--- app/src/pages/10_data_analyst_Home.py
+++ app/src/pages/10_data_analyst_Home.py
@@ -9,22 +9,22 @@ st.set_page_config(layout = 'wide')
 # Show appropriate sidebar links for the role of the currently logged in user
 SideBarLinks()
 
-st.title(f"Welcome USAID Worker, {st.session_state['first_name']}.")
+st.title(f"Welcome Data Analyst, {st.session_state['Phillip']}.")
 st.write('')
 st.write('')
 st.write('### What would you like to do today?')
 
-if st.button('Predict Value Based on Regression Model', 
+if st.button('Player Data Report', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/11_Prediction.py')
+  st.switch_page('pages/11_Player_Data_Report.py')
 
-if st.button('View the Simple API Demo', 
+if st.button('Team Data', 
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/12_API_Test.py')
+  st.switch_page('pages/12_Team_Data.py')
 
-if st.button("View Classification Demo",
+if st.button("Compare Players",
              type='primary',
              use_container_width=True):
-  st.switch_page('pages/13_Classification.py')
+  st.switch_page('pages/13_Compare_players.py')

92c885a0df7164565bfdb664e0a4489357cf5649
Esteban Puyana <puyanasalazar.e@northeastern.edu>
2025-04-14 17:21:12 -0400

    removed deprecated blueprints from registering

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -41,8 +41,6 @@ def create_app():
     # and give a url prefix to each
     app.logger.info('current_app(): registering blueprints with Flask app object.')   
     app.register_blueprint(simple_routes)
-    app.register_blueprint(customers,   url_prefix='/c')
-    app.register_blueprint(products,    url_prefix='/p')
     app.register_blueprint(players,     url_prefix='/pl')
     app.register_blueprint(users, url_prefix='/u')
     app.register_blueprint(gameplans, url_prefix='/gp')

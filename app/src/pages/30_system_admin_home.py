f2005ab301ffe0b9e2dda2aab810aa9378b67011
ramanang711 <127068368+ramanang711@users.noreply.github.com>
2025-04-14 00:01:34 -0400

    Transfered my system admin homepage to main

--- app/src/pages/30_system_admin_home.py
+++ app/src/pages/30_system_admin_home.py
@@ -3,15 +3,29 @@ logger = logging.getLogger(__name__)
 
 import streamlit as st
 from modules.nav import SideBarLinks
-import requests
 
-st.set_page_config(layout = 'wide')
+st.set_page_config(layout='wide')
 
+# Sidebar nav links based on session role
 SideBarLinks()
 
-st.title('System Admin Home Page')
+# Welcome section
+st.title(f"Welcome System Admin, {st.session_state['first_name']}")
+st.write('')
+st.write('### Admin Control Panel')
 
-if st.button('Update ML Models', 
-             type='primary',
-             use_container_width=True):
-  st.switch_page('pages/21_ML_Model_Mgmt.py')
+# Navigation Buttons
+if st.button('Update GM Info', use_container_width=True):
+    st.switch_page('pages/04_Update_GM_Info.py')
+
+if st.button('Add New Match', use_container_width=True):
+    st.switch_page('pages/04_Add_Match.py')
+
+if st.button('Add New GM', use_container_width=True):
+    st.switch_page('pages/04_Add_GM.py')
+
+if st.button('Update Match Score', use_container_width=True):
+    st.switch_page('pages/04_Update_Match_Score.py')
+
+if st.button('Delete User', use_container_width=True):
+    st.switch_page('pages/04_Delete_User.py')

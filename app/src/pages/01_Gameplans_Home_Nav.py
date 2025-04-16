38899d87546cb1c91ca9dfc9d68fb1e460e2ed1e
annieWong25 <anniewongboston@gmail.com>
2025-04-16 10:41:25 -0400

    coach report and gameplan pages

--- app/src/pages/01_Gameplans_Home_Nav.py
+++ app/src/pages/01_Gameplans_Home_Nav.py
@@ -1,44 +1,76 @@
 import logging
 logger = logging.getLogger(__name__)
+import datetime
 import streamlit as st
 import requests 
+import json
 from streamlit_extras.app_logo import add_logo 
 from modules.nav import SideBarLinks
 
 SideBarLinks()
 
-'''
-TEMPLATE
-st.write("# More API Access Examples")
-st.write("## Creating a New Product")
-'''
+
+# TEMPLATE
+# st.write("# More API Access Examples")
+# st.write("## Creating a New Product")
+
+
+# DELETE THIS BUTTON 
+#if st.button('View Selected Gameplan', 
+#             type='primary',
+#             use_container_width=True):
+#  st.switch_page('pages/01_Selected_Gameplan.py')
+
+# DELETE 01_New_Gameplan.py...
+#if st.button('Create New Gameplan', 
+#             type='primary',
+#             use_container_width=True):
+#  st.switch_page('pages/01_New_Gameplan.py')
+
+# TESTED 
 st.write("# View All Gameplans")
-st.write("## Or Make A New Gameplan!")
+# Simply retrieving data from a REST api running in a separate Docker Container.
+#If the container isn't running, this will be very unhappy.  But the Streamlit app 
+#should not totally die. 
 
+data = {} 
+try:
+  data = requests.get('http://api:4000/gp/gameplans').json()
+except:
+  st.write("**Important**: Could not connect to sample api, so using dummy data.")
+  data = {"a":{"b": "123", "c": "hello"}, "z": {"b": "456", "c": "goodbye"}}
 
-if st.button('View Selected Gameplan', 
-             type='primary',
-             use_container_width=True):
-  st.switch_page('pages/01_Selected_Gameplan.py')
+#st.dataframe(data)
+st.table(data)
 
-if st.button('Create New Gameplan', 
-             type='primary',
-             use_container_width=True):
-  st.switch_page('pages/01_New_Gameplan.py')
+# TESTED - creating a form 
+st.write("## Make a New Gameplan!")
 
-
-# DOES NOT WORK
-  # creating a form 
-  st.write("## Make a New Gameplan!")
-
-  with st.form("Make a New Gameplan!"):
+with st.form("Make a New Gameplan!"):
+    # today_date = datetime.date.today()
+    plan_date = st.date_input("Date created:", value=None) 
     plan_content = st.text_input("Describe New Gameplan:")
+    uploaded_files = st.file_uploader("Choose a file", accept_multiple_files=True)
+
+    # Insert a file uploader that accepts multiple files at a time
+    for uploaded_file in uploaded_files: 
+        bytes_data = uploaded_file.read()
+        st.write("filename:", uploaded_file.name)
+        # st.write(bytes_data) we do not need to see binary translation of image on screen
     
-    submitted = st.form_submitted_button("Submit")
+    submitted = st.form_submit_button("Submit")
 
     if submitted:
-        data = {}
+      data = {}
+      if not plan_date or not plan_content or not uploaded_files:
+        st.warning("Please fill all required fields: date, content, and file(s)")
+        data['gameplan_date'] = plan_date
+        # data['gameplan_files'] = uploaded_files
+      else:
+        data['gameplan_date'] = plan_date.isoformat()
         data['gameplan_content'] = plan_content
-        st.write(data)
+        # data['gameplan_files'] = uploaded_files.isoformat() error
+        st.write(data) #helpful to see if data was proccessed 
+        st.success("Gameplan successfully submitted", icon="✅")
 
-        requests.post('http://api:4000/p/product', json=data)
+        requests.post('http://api:4000/gp/gameplans', json=data)
--- app/src/pages/01_Reports.py
+++ app/src/pages/01_Reports.py
@@ -1,7 +1,33 @@
 import logging
 logger = logging.getLogger(__name__)
+import datetime
 import streamlit as st
 import requests
 from modules.nav import SideBarLinks
 
 SideBarLinks()
+
+# TESTED - creating a form 
+st.write("# Make a Report!")
+
+with st.form("Make a Report!"):
+    date = st.date_input("Date Created:", value=None) 
+    content = st.text_input("Describe New Report:")
+    
+    submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        data = {}
+        if not date or not content:
+            st.warning("Please fill all required fields: date, content")
+            data['report_date'] = date
+        else: 
+            data['report_date'] = date.isoformat()
+            data['repoty_content'] = content
+            st.write(data)
+            st.success("Report successfully submitted", icon="✅") 
+
+            requests.post('http://api:4000/gp/gameplans', json=data) 
+            #TODO change to report routes 
+            #TODO update reports page to show reports and make reports 
+        
